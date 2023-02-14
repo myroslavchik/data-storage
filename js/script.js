1833872ce80281057ef22c627708a42fda9e0668
Myroslav <myroslavtp@gmail.com>
2023-02-14 13:07:58 +0200

    new changes - added top btn, fades

---
+++ js/script.js
@@ -0,0 +1,39 @@
+//animation scroll
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+
+//up button
+
+const BTNUP = document.querySelector('.btn__up');
+BTNUP.addEventListener('click', toUp);
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    BTNUP.style.display = "flex";
+  } else {
+    BTNUP.style.display = "none";
+  }
+}
+
+function toUp () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
