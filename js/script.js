a4c6ac484dc52bcdae0db79279f4ce8f9f02abe9
Myroslav <myroslavtp@gmail.com>
2023-02-15 11:50:00 +0200

    dev

--- js/script.js
+++ js/script.js
@@ -11,12 +11,12 @@ const menu = document.querySelector('.topnav');
 //animation scroll
 
 function reveal() {
-    var reveals = document.querySelectorAll(".reveal");
+    let reveals = document.querySelectorAll(".reveal");
   
-    for (var i = 0; i < reveals.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = reveals[i].getBoundingClientRect().top;
-      var elementVisible = 150;
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
   
       if (elementTop < windowHeight - elementVisible) {
         reveals[i].classList.add("active");
